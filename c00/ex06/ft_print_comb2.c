d1875c71a8607507d269128d7bb49d883c6728b3
ChangHui-Song <sch102403@gmail.com>
2021-10-13 22:45:32 +0900

    fixed ex06 completed ex07 solving ex08

--- c00/ex06/ft_print_comb2.c
+++ c00/ex06/ft_print_comb2.c
@@ -3,13 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_comb2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chsong <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: sch <chsong@student.42seoul.kr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/12 18:00:45 by chsong            #+#    #+#             */
-/*   Updated: 2021/10/12 21:13:30 by chsong           ###   ########.fr       */
+/*   Created: 2021/10/13 01:10:25 by sch               #+#    #+#             */
+/*   Updated: 2021/10/13 12:56:38 by chsong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 void	ft_putchar(char num1, char num2);
 void	ft_print_comb2(void);
 
@@ -19,14 +20,48 @@ void	ft_print_comb2(void)
 	char	num2;
 
 	num1 = '0';
-	while (num1 <= '9')
+	num2 = '0';
+	while (num1 < '9' || num2 <= '8')
 	{
-		num2 = '0';
-		while (num2 <= '9')
+		if (num2 == '9')
 		{
 			ft_putchar(num1, num2);
-			num2++;
+			num1++;
+			num2 = '0';
 		}
-		num1++;
+		ft_putchar(num1, num2);
+		num2++;
 	}
+}
+
+void ft_putchar(char num1, char num2)
+{
+	char	n1;
+	char	n2;
+
+	n1 = num1;
+	n2 = num2 + 1;
+	while (n1 <= '9' && n2 <= '9')
+	{
+		write(1, &num1, 1);
+		write(1, &num2, 1);
+		write(1, " ", 1);
+		write(1, &n1, 1);
+		write(1, &n2, 1);
+		if (num1 != '9' || num2 != '8' || n1 != '9' || n2 != '9')
+			write(1, ", ", 2);
+		n2++;
+		if (n2 == ':')
+		{
+			n1++;
+			n2 = '0';
+
+		}
+	}
+}
+
+int main()
+{
+	ft_print_comb2();
+	return (0);
 }
---
+++ c00/ex08/ft_print_comb2.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+ft_print_combn(int n);
+ft_combination();
+
+ft_print_combn(int n)
+{
+	
+}
+
+int	main()
+{
+	ft_print_combn(2);
+	return (0);
+}
