32508d724a586885b313eb0b17c9eedc5d49061a
ChangHui-Song <sch102403@gmail.com>
2021-10-17 10:24:33 +0900

    solved ex03 ~ ex06

---
+++ c02/ex03/ft_str_is_numeric.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chsong <chsong@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/17 00:45:23 by chsong            #+#    #+#             */
+/*   Updated: 2021/10/17 00:46:16 by chsong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_number(char str)
+{
+	if (str >= '0' && str <= '9')
+		return (1);
+	return (0);
+}
+
+int	ft_str_is_numeric(char *str)
+{
+	int	cnt;
+
+	if (!*str)
+		return (1);
+	cnt = 0;
+	while (*(str + cnt))
+	{
+		if (!ft_is_number(*(str + cnt)))
+			return (0);
+		cnt++;
+	}
+	return (1);
+}
