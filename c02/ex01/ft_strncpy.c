cc4223d04fde30ad1ae0c38e759c5cb5486d8d55
ChangHui-Song <sch102403@gmail.com>
2021-10-22 22:13:43 +0900

    fixed c02 ex01 completed c06

--- c02/ex01/ft_strncpy.c
+++ c02/ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: chsong <chsong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/17 00:22:51 by chsong            #+#    #+#             */
-/*   Updated: 2021/10/21 20:13:54 by chsong           ###   ########.fr       */
+/*   Updated: 2021/10/22 22:12:51 by chsong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,15 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	unsigned int	cnt;
 
 	cnt = 0;
-	while (cnt < n)
+	while ((*(src + cnt) = '\0') && (cnt < n))
 	{
-		if (*(src + cnt))
-			*(dest + cnt) = *(src + cnt);
-		else
-			*(dest + cnt) = '\0';
+		*(dest + cnt) = *(src + cnt);
 		cnt++;
 	}
-	*(dest + cnt) = '\0';
+	while (cnt < n)
+	{
+		*(dest + cnt) = '\0';
+		cnt++;
+	}
 	return (dest);
 }
