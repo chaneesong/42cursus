739514127a611848847812c557a68c3c0cec62ee
ChangHui-Song <sch102403@gmail.com>
2021-10-24 03:01:08 +0900

    modified example

--- c07/ex02/ft_ultimate_range.c
+++ c07/ex02/ft_ultimate_range.c
@@ -1,7 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chsong <chsong@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/24 01:00:53 by chsong            #+#    #+#             */
+/*   Updated: 2021/10/24 01:00:55 by chsong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 
 int	ft_ultimate_range(int **range, int min, int max)
 {
 	int	*num_ptr;
 	int	num;
+
+	if (min >= max)
+		return (0);
+	num_ptr = (int *)malloc(sizeof(int) * (max - min));
+	num = 0;
+	while (min < max)
+	{
+		*(num_ptr + num) = min;
+		min++;
+	}
+	*range = num_ptr;
+	return (num);
 }
