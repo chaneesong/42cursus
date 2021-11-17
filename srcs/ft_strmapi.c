62dda1a7e4abef90b6e26e24d6f63e0ffb19b875
Changhee Song <chsong@c8r3s6.42seoul.kr>
2021-11-17 15:42:41 +0900

    20211117 added libft

---
+++ srcs/ft_strmapi.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chsong <chsong@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/17 15:17:10 by chsong            #+#    #+#             */
+/*   Updated: 2021/11/17 15:37:22 by chsong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    char    *tmp;
+    size_t  i;
+
+    tmp = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+    i = 0;
+    while (s[i])
+    {
+        tmp[i] = f(i, s[i]);
+        i++;
+    }
+    tmp[i] = '\0';
+    return (tmp);
+}
