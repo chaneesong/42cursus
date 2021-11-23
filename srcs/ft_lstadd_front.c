0ee39f93e9320218612b30b3d5a860d3a3667c1b
Changhee Song <chsong@c5r8s5.42seoul.kr>
2021-11-23 14:48:53 +0900

    updated 20211123

--- srcs/ft_lstadd_front.c
+++ srcs/ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: chsong <chsong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 17:09:11 by chsong            #+#    #+#             */
-/*   Updated: 2021/11/21 16:12:10 by chsong           ###   ########.fr       */
+/*   Updated: 2021/11/23 14:44:37 by chsong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,10 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	t_list	**tmp;
-
-	tmp = lst;
 	if (!lst)
 		lst = (t_list **)malloc(sizeof(t_list *));
 	if (!new)
 		new = ft_lstnew(NULL);
-	new->next = *tmp;
+	new->next = lst[0];
 	*lst = new;
 }
