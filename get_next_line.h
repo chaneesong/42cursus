908306ad5686a50af726a1a05a7cb3dfeca5943e
Changhee Song <chsong@c8r6s8.42seoul.kr>
2021-11-29 18:09:04 +0900

    updated 20211129

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chsong <chsong@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/29 18:04:42 by chsong            #+#    #+#             */
+/*   Updated: 2021/11/29 18:07:23 by chsong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#	ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#	endif
