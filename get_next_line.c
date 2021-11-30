be59b07fc898f862e4ad64ee22ce731338a3273d
ChangHui-Song <sch102403@gmail.com>
2021-12-01 00:38:18 +0900

    updated 20211130

--- get_next_line.c
+++ get_next_line.c
@@ -6,34 +6,74 @@
 /*   By: chsong <chsong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 18:03:38 by chsong            #+#    #+#             */
-/*   Updated: 2021/11/30 21:13:13 by chsong           ###   ########.fr       */
+/*   Updated: 2021/12/01 00:37:32 by chsong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
+size_t	ft_strndup(char **dest, char **src, size_t n)
+{
+	size_t	i;
 
+	*dest = (char *)ft_calloc(sizeof(char), (n + 1));
+	if (!dest)
+		return (0);
+	i = 0;
+	while (i < n)
+	{
+		(*dest)[i] = **src;
+		i++;
+		(*src)++;
+	}
+	(*dest)[i] = '\0';
+	return (i);
+}
+
+char	*ft_cut_str(char *s)
+{
+	char	*tmp;
+	size_t	len;
+	size_t	i;
+
+	len = ft_strlen(s);
+	tmp = (char *)malloc(sizeof(char) * (len + 1));
+	if (!tmp)
+		return (NULL);
+	while(i < len)
+	{
+		tmp[i] = s[i];
+		i++;
+	}
+	tmp[i] = '\0';
+	return (tmp);
+}
 
 char	*get_next_line(int fd)
 {
 	static char	*rest;
 	char		*tmp;
+	char		*res;
 	size_t		rsize;
 	
+	if (fd < 0)
+		return (NULL);
 	tmp = (char *)ft_calloc(sizeof(char), (BUFFER_SIZE + 1));
 	if (!tmp)
 		return (NULL);
-	rsize = read(fd, tmp, BUFFER_SIZE);
-	while (rsize)
+	while (ft_strchr(rest, '\n') == -1 && rsize)
 	{
-		rest = ft_strjoin(rest, tmp);
-		ft_memset(tmp, 0, BUFFER_SIZE + 1);
 		rsize = read(fd, tmp, BUFFER_SIZE);
+		rest = ft_strjoin(rest, tmp);
 	}
 	printf("rest=%s\n", rest);
 	if (ft_strchr(rest, '\n'))
-		rest[ft_strchr(rest, '\n')] = '1';
+	{
+		ft_strndup(&res, &rest, ft_strchr(rest, '\n') + 1);
+		printf("rest=%s\n", rest);
+		rest = ft_cut_str(rest);
+	}
 	free(tmp);
-	return (NULL);
+	return (res);
 }
