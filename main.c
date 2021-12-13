ea70d58ac32133ebe741ac575e488507711985a2
Changhee Song <chsong@c8r6s8.42seoul.kr>
2021-12-13 19:44:56 +0900

    updated 20211213

---
+++ main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chsong <chsong@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/13 14:35:38 by chsong            #+#    #+#             */
+/*   Updated: 2021/12/13 17:26:28 by chsong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <time.h>
+
+int test(int cnt, ...)
+{
+	int result = 0;
+	va_list arg_lst;
+
+	va_start(arg_lst, cnt);
+	for (int i = 0; i < cnt; i++)
+		result += va_arg(arg_lst, int);
+	va_end(arg_lst);
+	return (result);
+}
+
+int	main()
+{
+	printf("%u", -2);
+	return 0;
+}
