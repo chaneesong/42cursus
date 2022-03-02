0e26b98dadcac79c32060fcbf2618e396a27a48a
ChangHui-Song <sch102403@gmail.com>
2022-03-02 18:06:43 +0900

    메인 함수 불필요한 코드 제거

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: chsong <chsong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 16:56:13 by chsong            #+#    #+#             */
-/*   Updated: 2022/03/02 15:56:52 by chsong           ###   ########.fr       */
+/*   Updated: 2022/03/02 18:06:20 by chsong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,7 @@ int	main(int argc, char **argv)
 	t_node	*head;
 
 	head = create_node(0);
-	head->value = 0;
 	if(argc > 2)
 		parse_argv(&head, argv);
-	while (head->prev)
-	{
-		printf("value = %d\n", head->value);
-		head = head->prev;
-	}
 	return (0);
 }
