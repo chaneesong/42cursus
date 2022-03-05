ea77c473f79ff866acde5ef3ceea5e87e371313c
ChangHui-Song <sch102403@gmail.com>
2022-03-05 19:50:48 +0900

    파싱 단계에서 push_a -> push_init 함수로 수정

--- init/parse_argv.c
+++ init/parse_argv.c
@@ -6,11 +6,26 @@
 /*   By: chsong <chsong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 14:56:17 by chsong            #+#    #+#             */
-/*   Updated: 2022/03/05 16:06:08 by chsong           ###   ########.fr       */
+/*   Updated: 2022/03/05 18:44:17 by chsong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
+
+static void	push_init(t_list **stack, t_node *current)
+{
+	if((*stack)->a_size == 0)
+	{
+		(*stack)->a_top = current;
+		(*stack)->a_bottom = current;
+		(*stack)->a_size++;
+		return ;
+	}
+	(*stack)->a_bottom->prev = current;
+	current->next = (*stack)->a_bottom;
+	(*stack)->a_bottom = current;
+	(*stack)->a_size++;
+}
 
 t_list	*parse_argv(char **argv)
 {
@@ -26,7 +41,7 @@ t_list	*parse_argv(char **argv)
 		while (split && *split)
 		{
 			current = create_node(ft_atoi(*split));
-			push_a(&stack, current);
+			push_init(&stack, current);
 			split++;
 		}
 		argv++;
