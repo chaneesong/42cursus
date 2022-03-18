0537eaacd1a7a315a5f38e4c6d4e3cfcfd9b4211
ChangHui-Song <sch102403@gmail.com>
2022-03-18 16:58:45 +0900

    restart

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -5,45 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: chsong <chsong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/25 16:56:13 by chsong            #+#    #+#             */
-/*   Updated: 2022/03/11 16:50:45 by chsong           ###   ########.fr       */
+/*   Created: 2022/03/18 16:53:16 by chsong            #+#    #+#             */
+/*   Updated: 2022/03/18 16:57:44 by chsong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	check_null(char **argv)
+int	main (int argc, char *argv[])
 {
-	while (*argv)
-	{
-		if (*argv[0] == 0)
-			return (1);
-		argv++;
-	}
-	return (0);
-}
-
-int	main(int argc, char **argv)
-{
-	t_list	*stack;
-	t_node	*tmp;
-
-	if (argc == 1)
-		return (0);
-	if (argc < 2 || check_null(argv))
-	{
-		print_error();
-		exit(-1);
-	}
-	stack = parse_argv(argv);
-	sort_stack(stack);
-	tmp = stack->a_top;
-	printf("stack A = ");
-	while (tmp)
-	{
-		printf("%d ", tmp->value);
-		tmp = tmp->prev;
-	}
-	printf("\n");
 	return (0);
 }
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -5,12 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: chsong <chsong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/25 16:56:38 by chsong            #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2022/03/11 16:38:34 by chsong           ###   ########.fr       */
-=======
-/*   Updated: 2022/03/10 22:02:37 by chsong           ###   ########.fr       */
->>>>>>> 0b7f38403b93443ad4645eb842c1901aaa5ebb4e
+/*   Created: 2022/03/18 16:53:30 by chsong            #+#    #+#             */
+/*   Updated: 2022/03/18 16:57:05 by chsong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +19,6 @@
 
 typedef struct s_node
 {
-	struct s_node	*prev;
 	int				value;
 	struct s_node	*next;
 }	t_node;
@@ -40,45 +35,6 @@ typedef struct s_list
 	struct s_node	*b_bottom;
 }	t_list;
 
-/* main function*/
-int		main(int argc, char *argv[]);
-
-/*init part*/
-t_list	*parse_argv(char **argv);
-t_node	*create_node(int value);
-void	push_a(t_list *stack, t_node *node);
-void	push_b(t_list *stack, t_node *node);
-t_node	*pop_a(t_list *stack);
-t_node	*pop_b(t_list *stack);
-
-/* operation part*/
-void	sb(t_list *stack);
-void	sa(t_list *stack);
-void	ss(t_list *stack);
-void	pa(t_list *stack);
-void	pb(t_list *stack);
-void	ra(t_list *stack);
-void	rb(t_list *stack);
-void	rr(t_list *stack);
-void	rra(t_list *stack);
-void	rrb(t_list *stack);
-void	rrr(t_list *stack);
-
-/*utils part*/
-int		ft_atoi(const char *str);
-int		ft_isdigit(int c);
-char	**ft_split(char const *s, char c);
-void	*ft_calloc(size_t count, size_t size);
-void	print_error(void);
-
-void	sort_stack(t_list *stack);
-void	sort_two_node(t_list *stack);
-void	sort_three_node(t_list *stack);
-void	find_pivot(t_list *stack);
-void	sort_big_size(t_list *stack);
-int		return_value(int total, int cur_size);
-int		low_value(t_list *stack, t_node *current);
-int		high_value(t_list *stack, t_node *current);
-int		upsign(int value);
+int	main(int argc, char *argv[]);
 
 #endif
